ca2f61b416ebf9d15dd9fe2860ae94f00c256209
andrepl <andrepleblanc@gmail.com>
2014-05-04 12:46:34 -0400

    move script to an entry_point

--- rival/rivalctl.py
+++ rival/rivalctl.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python
+
 from __future__ import print_function
+
 import sys
 import os
 try:
@@ -16,15 +18,15 @@ def send_reports(reports):
     for report in reports:
         send(report)
 
-def main(args):
+def _main(args):
     reports = []
     if args.reset:
         reports = FACTORY_PROFILE.to_report_list()
     if args.profile:
-    	profile = Profile.find_profile(args.profile)
-    	print("Loading profile %s" % (os.path.splitext(profile)[0],))
-    	profile = Profile.from_yaml(open(profile))
-    	reports = profile.to_report_list()
+        profile = Profile.find_profile(args.profile)
+        print("Loading profile %s" % (os.path.splitext(profile)[0],))
+        profile = Profile.from_yaml(open(profile))
+        reports = profile.to_report_list()
 
     if args.logo_color is not None:
         reports.append(set_logo_color(args.logo_color))
@@ -45,21 +47,22 @@ def main(args):
     send_reports(reports)
 
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description="A tool to configure the SteelSeries Rival Gaming Mouse")
 parser.add_argument('--commit', help="Save to firmware", default=False, action='store_true')
 parser.add_argument("--reset", help="Reset all options to FACTORY defaults", action='store_true', default=False)
 parser.add_argument("--wheel-color", type=str, metavar="COLOR", help="any valid css color name or hex string")
-parser.add_argument("--wheel-style", type=int, metavar="STYLE", help="LED Style [1=Steady, 2-4=Breathe Speed]")
+parser.add_argument("--wheel-style", type=int, metavar="STYLE", help="LED Style [1=Steady, 2-4=Breathe Speed]", choices=[1,2,3,4])
 parser.add_argument("--logo-color", type=str, metavar="COLOR", help="any valid css color name or hex string")
-parser.add_argument("--logo-style", type=int, metavar="STYLE", help="LED Style [1=Steady, 2-4=Breathe Speed]")
+parser.add_argument("--logo-style", type=int, metavar="STYLE", help="LED Style [1=Steady, 2-4=Breathe Speed]", choices=[1,2,3,4])
 
 parser.add_argument("--cpi1", type=int, metavar="CPI", help="50-6500 in increments of 50 [default 800]")
 parser.add_argument("--cpi2", type=int, metavar="CPI", help="50-6500 in increments of 50 [default 1600]")
 parser.add_argument("--profile", type=str, metavar="PROFILE", help="profile name or path to file")
-parser.add_argument("--polling-rate", metavar="RATE", type=int, help="1000, 500, 250, or 125 [default=1000]")
+parser.add_argument("--polling-rate", metavar="RATE", type=int, help="1000, 500, 250, or 125 [default=1000]", choices=[1000,500,250,125])
 
-if __name__ == '__main__':
-    if len(sys.argv)==1:
+def main():
+    if len(sys.argv) == 1:
         parser.print_help()
     args = parser.parse_args()
-    main(args)
+    _main(args)
+    return 0
--- setup.py
+++ setup.py
@@ -1,20 +1,30 @@
 from setuptools import setup, find_packages
 setup(
+
     name = "rivalctl",
     version = "0.1",
     packages = find_packages(),
-    scripts = ['bin/rivalctl'],
+
+    entry_points = {
+        'console_scripts': [
+            'rivalctl = rival.rivalctl:main'
+        ]
+    },
 
     # metadata for upload to PyPI
     author = "Andre P. LeBlanc",
     author_email = "andrepleblanc@gmail.com",
     description = "A tool to configure the SteelSeries Rival Gaming Mouse",
+
     license = "GPL2",
     keywords = "steelseries rival",
     url = "https://github.com/andrepl/rivalctl/",
+
     install_requires=[
         'ioctl-opt>=1.2',
         'pyudev>=0.16',
-        'webcolors>=1.4'
+        'webcolors>=1.4',
+        'pyyaml>=3.10'
     ],
+
 )
